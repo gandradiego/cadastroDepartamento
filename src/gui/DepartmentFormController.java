604a17f1c1267e032c508967aa88fb411745d590
gandradiego <diegogandra21@gmail.com>
2020-12-22 17:08:26 -0300

    Implementação das funcionalidades para salvar um novo departamento e implementação dos métodos dos daos

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,17 +1,31 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
+	private Department entity;
+	private DepartmentService service;
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<DataChangeListener>();
+	
 	@FXML
 	private TextField txtFieldId;
 	@FXML
@@ -24,13 +38,38 @@ public class DepartmentFormController implements Initializable {
 	private Button btnCancel;
 	
 	@FXML
-	public void onBtnSaveAction() {
-		System.out.println("Botão salvar");
+	public void onBtnSaveAction(ActionEvent event) {
+		if(service == null)
+			throw new IllegalStateException("Service was null");
+		
+		entity = getEntityFromForm();
+		
+		try {
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Erro ao salvar o departamento", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners)
+			listener.onDataChanged();
+	}
+
+	private Department getEntityFromForm() {
+		Department department = new Department();
+		department.setId(Utils.tryParseToInt(txtFieldId.getText()));
+		department.setName(txtFieldName.getText());
+		
+		return department;
+	}
+
 	@FXML
-	public void onBtnCancelAction() {
-		System.out.println("Botão cancelar");
+	public void onBtnCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
@@ -43,4 +82,23 @@ public class DepartmentFormController implements Initializable {
 		Constraints.setTextFieldMaxLength(txtFieldName, 30);
 	}
 
+	public void setEntity(Department entity) {
+		this.entity = entity;
+	}
+	
+	public void setService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+
+	public void updateFormData() {
+		if(entity == null)
+			throw new IllegalArgumentException("Entity was null");
+		
+		txtFieldId.setText(String.valueOf(entity.getId()));
+		txtFieldName.setText(entity.getName());
+	}
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,6 +1,7 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,33 +9,111 @@ import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
 	@Override
 	public void insert(Department department) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO department(Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, department.getName());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				
+				if(rs.next())
+					department.setId(rs.getInt(1));
+				
+				DB.closeResultSet(rs);
+			}
+			else
+				throw new DbException("Unexpected error: no rows affected.");
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void update(Department department) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+ "SET Name = ? "
+					+ "WHERE Id = ?");
+			st.setString(1, department.getName());
+			st.setInt(6, department.getId());
+			
+			st.executeUpdate();
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if(rs.next()) 
+				return instantiateDepartment(rs);;
+			
+			return null;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
@@ -60,9 +139,16 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
-			DB.closeConnection();
 		}
 		
 		return departmentList;
+	}
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department department = new Department();
+		department.setId(rs.getInt("DepartmentId"));
+		department.setName(rs.getString("DepName"));
+		
+		return department;
 	}
 }
